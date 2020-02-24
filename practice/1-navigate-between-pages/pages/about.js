50406dabdb6f774158efc49e266be553ce3e042e
covee <uniqtop91@gmail.com>
2020-02-24 19:37:27 +0900

    .

--- practice/1-navigate-between-pages/pages/about.js
+++ practice/1-navigate-between-pages/pages/about.js
@@ -1,8 +1,9 @@
+import Layout from '../components/MyLayout'
+
 export default function About() {
   return (
-    <div>
-      <p>This is the about page.</p>
-      <p>Error Fix...</p>
-    </div>
+    <Layout>
+      <p>This is the about page!</p>
+    </Layout>
   )
 }
